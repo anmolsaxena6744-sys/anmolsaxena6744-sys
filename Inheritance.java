ac67d1b51b9dea592a254983d285630706edd552
Anmol Saxena <anmolsaxena6744@gmail.com>
2026-07-29 23:02:50 +0530

    Implement inheritance with Base and Derived classes

--- Inheritance.java
+++ Inheritance.java
@@ -1,1 +1,40 @@
+// Inheritance code
+class Base{
+  public int x;
+  public int getx(){
+    return x;
+  }
+  public void setx(int x){
+    System.out.println("I am in base and setting x now");
+    this.x = x;
+  }
+  public void printme(){
+    System.out.println("I am a constructor");
+  }
+}
 
+class Derived extends Base {
+  public int y;
+
+  public int gety(){
+  return y;
+  }
+
+  public void sety(int y){
+    this.y = y;
+  }
+}
+public class Main{
+  public static void main(String[] args){
+
+    Base b = new Base();
+    b.setx(21);
+    System.out.println(b.getx());
+
+    Derived a = new Derived();
+    a.sety(43);
+    System.out.println(a.gety());
+  }
+}
+    
+  
