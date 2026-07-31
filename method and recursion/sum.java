39b25b9541c99d60d57ee5d14ed26f7f56267e13
Anmol Saxena <anmolsaxena6744@gmail.com>
2026-07-31 03:58:22 -0700

    Create sum.java

---
+++ method and recursion/sum.java
@@ -0,0 +1,14 @@
+class Main{
+static int Sum(int n){
+    if(n == 1)
+        return 1;
+       
+            return n + Sum(n-1);
+        
+    }
+
+
+public static void main(String[] args){
+   System.out.println(Sum(3));
+}
+}
