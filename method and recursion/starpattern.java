256f8d106354464c9b5eaebd3b959ae701045a77
Anmol Saxena <anmolsaxena6744@gmail.com>
2026-07-31 03:49:48 -0700

    Create starpattern.java

---
+++ method and recursion/starpattern.java
@@ -0,0 +1,15 @@
+class Main{
+static void Pattern(int n){
+    for(int i = 1 ; i<=n ; i++){
+        for(int j = 1 ; j<= i ; j++){
+            System.out.print("*");
+        }
+          System.out.println();
+        
+        
+    }
+}
+public static void main(String[] args){
+    Pattern(4);
+}
+}
