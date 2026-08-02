0c93ac48c8b4a9f5c2de6a67e486843a135cf94c
Anmol Saxena <anmolsaxena6744@gmail.com>
2026-08-02 12:27:11 +0530

    Add Celsius to Fahrenheit conversion method

---
+++ method and recursion/celcius_to_fahrenheit.java
@@ -0,0 +1,9 @@
+class Main{
+    static float fahrenheit(float c){
+        return (float) (c * 9/5) + 32;
+        
+    }
+    public static void main(String[] args){
+       System.out.println(fahrenheit(4));
+    }
+}
