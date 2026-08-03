96bfd99dc7223fb7bf3351204a23fd27d8f23005
Anmol Saxena <anmolsaxena6744@gmail.com>
2026-08-04 00:24:06 +0530

    Add TommyVecetti class with actions

---
+++ practice set_OOP/TommyVecetti.java
@@ -0,0 +1,20 @@
+class TommyVecetti{
+    void Hit(){
+        System.out.println("Hitting.....");
+    }
+    void Fire(){
+        System.out.println("Firing.....");
+        }
+    void Run(){
+        System.out.println("Running.....");
+        }
+        
+}
+class Main{
+    public static void main(String[] args){
+        TommyVecetti scr = new TommyVecetti();
+        scr.Hit();
+        scr.Fire();
+        scr.Run();
+    } 
+}
