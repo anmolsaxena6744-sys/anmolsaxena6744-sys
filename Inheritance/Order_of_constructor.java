4f6c2a049f7db0231b7c362a028f9990b12711dc
Anmol Saxena <anmolsaxena6744@gmail.com>
2026-08-06 22:58:45 +0530

    Add classes to demonstrate constructor order

---
+++ Inheritance/Order_of_constructor.java
@@ -0,0 +1,21 @@
+class Base{
+    Base(){
+        System.out.println("Base Constructor......");
+    }
+}
+class Derived1 extends Base{
+    Derived1(){
+        System.out.println("Derived1 Constructor......");
+    }
+}
+class Derived2 extends Derived1{
+    Derived2(){
+        System.out.println("Derived2 Constructor......");
+    }
+}
+class Main{
+    public static void main(String[] args){
+        Derived2 scr = new Derived2();
+        
+    }
+}
