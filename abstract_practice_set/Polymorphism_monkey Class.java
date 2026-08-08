3ed17b96a6940bd6da7b8a1b9d2226dd567cd8f6
Anmol Saxena <anmolsaxena6744@gmail.com>
2026-08-08 10:40:48 +0530

    Add Polymorphism example with Monkey and Human classes

---
+++ abstract_practice_set/Polymorphism_monkey Class.java
@@ -0,0 +1,30 @@
+interface BasicAnimal{
+    void eat();
+    void sleep();
+}
+class Monkey{
+    void jump(){
+        System.out.println("jumpinng.....");
+    }
+    void bite(){
+        System.out.println("bitingggg.....");
+    }
+}
+class Human extends Monkey implements BasicAnimal{
+    public void eat(){
+        System.out.println("eatinggg.....");
+    }
+  public void sleep(){
+       System.out.println("sleeping.....");
+   }
+}
+class Main{
+    public static void main(String[] args){
+        Monkey h = new Human();
+        h.jump();
+        h.bite();
+       // h.eat(); --> error because reference type is Monkey
+        
+    }
+}
+   
