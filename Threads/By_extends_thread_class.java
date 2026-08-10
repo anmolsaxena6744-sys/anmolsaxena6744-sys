2fb4c77ce5b83d2a33340d3c768a18828740a5f8
Anmol Saxena <anmolsaxena6744@gmail.com>
2026-08-10 15:28:29 +0530

    Add multithreading example with MyThread classes

---
+++ Threads/By_extends_thread_class.java
@@ -0,0 +1,31 @@
+class MyThread1 extends Thread {
+    @Override
+    public void run() {
+        int i = 0;
+        while (i < 400) {
+            System.out.println("I am Cooking");
+            i++;
+        }
+    }
+}
+
+class MyThread2 extends Thread {
+    @Override
+    public void run() {
+        int i = 0;
+        while (i < 400) {
+            System.out.println("I am Typing");
+            i++;
+        }
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        MyThread1 t1 = new MyThread1();
+        MyThread2 t2 = new MyThread2();
+
+        t1.start();
+        t2.start();
+    }
+}
